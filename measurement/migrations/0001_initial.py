5698ee45b59197f1bde754a898100da85bb7eef0
Ian Shurmer <git@i-sh.me.uk>
2013-11-18 09:09:50 +0000

    Back to Django migrations.

--- measurement/migrations/0001_initial.py
+++ measurement/migrations/0001_initial.py
@@ -10,8 +10,8 @@ class Migration(migrations.Migration):
     operations = [
         migrations.CreateModel(
             name = 'TestDistanceModel',
-            bases = (models.Model,),
-            fields = [('id', models.AutoField(serialize=False, auto_created=True, verbose_name='ID', primary_key=True),), ('distance', measurement.fields.DistanceField(),), ('distance_units', models.CharField(db_index=True, max_length=8),)],
             options = {},
+            fields = [('id', models.AutoField(verbose_name='ID', primary_key=True, auto_created=True, serialize=False),), ('distance', measurement.fields.DistanceField(),), ('distance_units', models.CharField(max_length=8, db_index=True),)],
+            bases = (models.Model,),
         ),
     ]
